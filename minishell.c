7a23479a39db57ce91a68040879fdac76ee68696
danielquintao <danielquintaodemoraes@gmail.com>
2023-04-16 23:14:10 -0300

    check file existence + program execution (no argv)

--- minishell.c
+++ minishell.c
@@ -1,12 +1,16 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include <stdio.h>  // TODO remove me
 
 int main() {
 
     while(1) {
-        // 1. read and parse cmd
+        // read cmd
         char *cmd;
         cmd = malloc(256);
         char *prog, **argv, *tmp;
@@ -27,6 +31,7 @@ int main() {
             break;
         }
 
+        // parse cmd
         prog = strtok(cmd, delim);
         if(prog) {
             printf("comando: %s\n", prog);
@@ -34,11 +39,38 @@ int main() {
         
         while(tmp = strtok(NULL, " ")) {
             argv = realloc(argv, (argc + 1) * sizeof *argv);
-            argv[argc] = malloc(sizeof tmp);
+            // argv[argc] = malloc(sizeof tmp);
             argv[argc++] = tmp;
         }
 
         for(int i = 0; i < argc; i++) printf("arg: %s\n", argv[i]);
+
+        // check if program file exists
+        struct stat sb;
+        int res = stat(prog, &sb);
+        if(res == -1 && errno == 2) {
+            printf("No such file\n");
+            continue;
+        } else if(res == -1) {
+            printf("Some error occurred while checking for file %s\n", prog);
+            continue;
+        } else if((sb.st_mode & S_IFMT) != S_IFREG) {
+            printf("%s is not a regular file\n", prog);
+            continue;
+        }
+
+        // execute program
+        pid_t pid = fork();
+
+        if(pid != 0) {
+            // we are in parent
+            int * wstatus;
+            wait(wstatus);
+        } else {
+            char * const nullargv[] = {NULL};
+            char * const nullenvp[] = {NULL};
+            execve(prog, nullargv, nullenvp); // TODO use argv
+        }
 
         // free memory
         free(cmd); // free(prog) is unnecessary, since prog points to an adress "mallocated" by cmd
